563a08d4a800f9d38752a6926fc7e88e3dde0a88
henri <henri1i.dev@gmail.com>
2022-09-02 16:09:25 -0300

    feat: create function to get the shortest path

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,14 @@ use std::{collections::{HashMap, LinkedList}, hash::Hash, fmt::Debug};
 pub struct Search<'a, T: Hash + Eq + Debug> {
     pub explored_path: HashMap<&'a T, &'a T>,
     pub target_vertex: &'a T,
+    initial_vertex: &'a T,
 }
 
 impl<'a, T: Hash + Eq + Debug> Search<'a, T> {
-    pub fn from<F>(graph: &'a HashMap<T, Vec<T>>, initial: &'a T, condition: F) -> Option<Self>
+    pub fn from<F>(graph: &'a HashMap<T, Vec<T>>, initial_vertex: &'a T, condition: F) -> Option<Self>
         where F: Fn(&'a T) -> bool,
     {
-        let mut queue = LinkedList::from([initial]);
+        let mut queue = LinkedList::from([initial_vertex]);
         let mut explored = HashMap::new();
 
         while !queue.is_empty() {
@@ -26,6 +27,7 @@ impl<'a, T: Hash + Eq + Debug> Search<'a, T> {
                     return Some(Self {
                         explored_path: explored,
                         target_vertex: adj_vertex,
+                        initial_vertex
                     });
                 }
 
@@ -34,5 +36,21 @@ impl<'a, T: Hash + Eq + Debug> Search<'a, T> {
         }
 
         None
+    }
+
+    pub fn path(&self) -> LinkedList<&T> {
+        let mut path = LinkedList::new();
+
+        let mut next_key = self.target_vertex;
+
+        while next_key != self.initial_vertex {
+            path.push_front(next_key);
+
+            next_key = self.explored_path.get(next_key).unwrap();
+        }
+
+        path.push_front(self.initial_vertex);
+
+        path
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ pub fn main() {
     });
 
     match result {
-        Some(search_result) => println!("Explored path: {:#?}", search_result.explored_path),
+        Some(search_result) => println!("Explored path: {:#?}", search_result.path()),
         None => println!("Person not found :(")
     }
 }
